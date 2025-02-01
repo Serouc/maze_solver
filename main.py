a4664d1cf9d9b5fbb4d279e444ec121691f6af35
Srouc <scscourtenay@gmail.com>
2025-02-01 22:26:32 +1100

    initial setup through to drawing lines

---
+++ main.py
@@ -0,0 +1,53 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title("aMAZing")
+        self.canvas = Canvas(self.__root, width = width, height = height)
+        self.canvas.pack()
+        self.running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+    
+    def wait_for_close(self):
+        self.running = True
+        while self.running == True:
+            self.redraw()
+
+    def close(self):
+        self.running = False
+    
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
+
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line():
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+    
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2
+            )
+
+
+def main():
+    win = Window(800, 600)
+    line = Line(Point(10, 20), Point(50, 100))
+    line2 = Line(Point(800, 0), Point(0, 600))
+    win.draw_line(line, "green")
+    win.draw_line(line2, "blue")
+    win.wait_for_close()
+
+main()
